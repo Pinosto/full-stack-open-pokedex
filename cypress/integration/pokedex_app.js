841e967eae14e91f117318375080bea974270a47
Pinosto <juho.pinosto@hotmail.com>
2021-04-13 15:24:40 +0300

    11.12

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -10,4 +10,10 @@ describe('Pokedex', function() {
     cy.contains('chlorophyll')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('bulbasaur page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('bulbasaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
 })
